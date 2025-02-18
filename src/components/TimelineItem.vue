5bf9799b927018e930615e705f3103d33a2d64f7
LADsy8 <dube.arthur@gmail.com>
2025-02-18 08:51:01 -0500

    first commit

---
+++ src/components/TimelineItem.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+/// <reference types="../../node_modules/.vue-global-types/vue_3.5_false.d.ts" />
+import type { TimelinePost } from '../posts';
+defineProps<{
+post: TimelinePost
+}>()</script>
+<template><li>
+    <span>{{ post.title }}</span>&nbsp;|&nbsp;
+    <span>{{ post.created.toFormat("d MMM") }}</span>
+    </li></template>
+<style></style>
